8dc0ca61a7c25d8e8482f3f1b65282e4dcf09faa
horita174 <reiuri0725@gmail.com>
2023-05-20 14:48:23 +0900

    課題後

--- text_excel.py
+++ text_excel.py
@@ -4,6 +4,10 @@ wb = Workbook()
 # grab the active worksheet
 ws = wb.active
 
+ws1 = wb.create_sheet("Mysheet",0)
+
+
+
 # Data can be assigned directly to cells
 ws['A1'] = 42
 
@@ -13,6 +17,12 @@ ws.append([1, 2, 3])
 # Python types will automatically be converted
 import datetime
 ws['A2'] = datetime.datetime.now()
+
+cell = ["A","B","C","D","E","F","G","H","I","J"]
+
+for x in range(1,11):
+        for y in range(1,11):
+            ws1[cell[y - 1] + str(x)] = x * y
 
 # Save the file
 wb.save("sample.xlsx")
